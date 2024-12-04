1de468594fd54c5547a6a1b44308a9da6afcbef8
jimcody2014 <jcodygroup@gmail.com>
2024-12-04 07:04:09 -0600

    added functions

---
+++ jim_functions/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='jim_functions',
+    packages=find_packages(include=['jim_functions']),
+    #install_requires=['pandas', 'numpy'],
+    version='0.1.0',
+    description='My first Python library',
+    author='Jim Cody',
+)
